8efb110881053f202f3b298da10fc7b7ea67c073
cantdocpp <cihuyman95@gmail.com>
2018-03-06 17:21:29 +0700

    selection sort

--- binary.js
+++ binary.js
@@ -2,14 +2,33 @@ const testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8];
 const testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55];
 
 function ownSort(arr) {
-  // Your sorting code
-  return arr;
+   for (var i = 0; i < arr.length; i++) {
+     for (var j = i; j < arr.length; j++) {
+       if (arr[j] < arr[i]) {
+         var temp = arr[i];
+         arr[i] = arr[j];
+         arr[j] = temp;
+       }
+     }
+   }
+   return arr
 }
 
 function binarySearch(search, array) {
-  // Your searching code
-  return 0;
+  var min = 0;
+  var max = array.length - 1;
+  var guess = Math.floor(max / 2);
+
+  while (array[guess] != search) {
+    if (search < array[guess]) {
+      array.slice(guess, max);
+    } else if (search > array[guess]) {
+      array.slice(min, guess)
+    }
+  }
+  return array
 }
+
 
 const arrayGenapSorted = ownSort(testArrayGenap);
 const arrayGanjilSorted = ownSort(testArrayGanjil);
@@ -23,6 +42,6 @@ console.log(binarySearch(53, arrayGanjilSorted));
 console.log(binarySearch(3, arrayGanjilSorted));
 console.log(binarySearch(2, arrayGanjilSorted));
 
-module.exports = {
-  binary_search
-};
+// module.exports = {
+//   binary_search
+// };
