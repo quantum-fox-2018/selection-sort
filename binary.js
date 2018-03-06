9835b9473c17e249f44c004cdbba863b73d18f82
davidjoshua87 <davidjosh_87@yahoo.com>
2018-03-06 17:45:27 +0700

    selection-sort

--- binary.js
+++ binary.js
@@ -1,28 +1,24 @@
 const testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8];
 const testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55];
 
+
 function ownSort(arr) {
   // Your sorting code
+  for (var i = 0; i < arr.length; i++) {
+    var min = i;
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    var first = arr[i];
+    arr[i] = arr[min];
+    arr[min] = first;
+  }
   return arr;
 }
-
-function binarySearch(search, array) {
-  // Your searching code
-  return 0;
-}
-
 const arrayGenapSorted = ownSort(testArrayGenap);
 const arrayGanjilSorted = ownSort(testArrayGanjil);
 
-// Driver code
-console.log(binarySearch(8, arrayGenapSorted));
-console.log(binarySearch(10, arrayGenapSorted));
-console.log(binarySearch(33, arrayGenapSorted));
-
-console.log(binarySearch(53, arrayGanjilSorted));
-console.log(binarySearch(3, arrayGanjilSorted));
-console.log(binarySearch(2, arrayGanjilSorted));
-
-module.exports = {
-  binary_search
-};
+console.log(ownSort(testArrayGenap));
+console.log(ownSort(testArrayGanjil));
