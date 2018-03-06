7c797ccefbe98bf3a4644e8e88d42807fe4bbb54
Fitrul Islam <fitrul.islam@gmail.com>
2018-03-06 15:50:25 +0700

    add selection sort

--- binary.js
+++ binary.js
@@ -1,13 +1,27 @@
 function selectionSort(input){
-  for(let i=1; i<input.length; i++){
-    for(let j=0; j<=i-1; j++){
-      let front = input[j];
-      let back = input[i];
-      if(input[i]<input[j]){
-        input[i] = front;
-        input[j] = back;
+  // for(let i=1; i<input.length; i++){
+  //   for(let j=0; j<=i-1; j++){
+  //     let front = input[j];
+  //     let back = input[i];
+  //     if(input[i]<input[j]){
+  //       input[i] = front;
+  //       input[j] = back;
+  //     }
+  //   }
+  // }
+  // return input;
+
+  for(let i=0; i<input.length; i++){
+    let lowestNumber = input[i];
+    for(let j=i+1; j<input.length; j++){
+      var indexForLowest;
+      if(input[j]<lowestNumber){
+        lowestNumber = input[j];
+        indexForLowest = j;
       }
     }
+    input[indexForLowest] = input[i];
+    input[i] = lowestNumber
   }
   return input;
 }
