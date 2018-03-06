18600d9852da8fe2675a13340093246deb575444
Taufik Hidayat <ovick_kd@yahoo.com>
2018-03-06 22:52:48 +0700

    selection sort

--- binary.js
+++ binary.js
@@ -1,28 +1,31 @@
-const testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8];
-const testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55];
+'use strict'
+
+var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
+var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
-  return arr;
+
+    for (let i = 0; i < arr.length; i++) {
+
+      for (let j = i; j < arr.length; j++) {
+
+        let temp = arr[i]
+
+        if (arr[i] > arr[j]) {
+
+          arr[i] = arr[j]
+          arr[j] = temp
+
+        }
+
+      }
+
+    }
+
+  console.log(arr);
+
 }
 
-function binarySearch(search, array) {
-  // Your searching code
-  return 0;
-}
-
-const arrayGenapSorted = ownSort(testArrayGenap);
-const arrayGanjilSorted = ownSort(testArrayGanjil);
-
-// Driver code
-console.log(binarySearch(8, arrayGenapSorted));
-console.log(binarySearch(10, arrayGenapSorted));
-console.log(binarySearch(33, arrayGenapSorted));
-
-console.log(binarySearch(53, arrayGanjilSorted));
-console.log(binarySearch(3, arrayGanjilSorted));
-console.log(binarySearch(2, arrayGanjilSorted));
-
-module.exports = {
-  binary_search
-};
+var arrayGenapSorted = ownSort(testArrayGenap)
+var arrayGanjilSorted = ownSort(testArrayGanjil)
