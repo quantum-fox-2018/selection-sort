6b574eab3f4b9f809d322fcbd0840e85a3de748c
Fitrul Islam <fitrul.islam@gmail.com>
2018-03-06 18:18:09 +0700

    add bubble and sortation

--- binary.js
+++ binary.js
@@ -1,4 +1,5 @@
 function selectionSort(input){
+  // insertion
   // for(let i=1; i<input.length; i++){
   //   for(let j=0; j<=i-1; j++){
   //     let front = input[j];
@@ -11,20 +12,39 @@ function selectionSort(input){
   // }
   // return input;
 
-  for(let i=0; i<input.length; i++){
-    let lowestNumber = input[i];
-    for(let j=i+1; j<input.length; j++){
-      var indexForLowest;
-      if(input[j]<lowestNumber){
-        lowestNumber = input[j];
-        indexForLowest = j;
+  //selection
+  // for(let i=0; i<input.length; i++){
+  //   let lowestNumber = input[i];
+  //   for(let j=i+1; j<input.length; j++){
+  //     var indexForSwap;
+  //     if(input[j]<lowestNumber){
+  //       lowestNumber = input[j];
+  //       indexForSwap = j;
+  //     }
+  //   }
+  //   if(lowestNumber!==input[i]){
+  //     input[indexForSwap] = input[i];
+  //     input[i] = lowestNumber;
+  //   }
+  // }
+  // return input;
+
+  //bubble
+  var swapped;
+  do{
+    swapped = false;
+    for(let i=0; i<input.length; i++){
+      let front = input[i];
+      let back = input[i+1];
+      if(input[i+1]<input[i]){
+        input[i+1] = front;
+        input[i] = back;
+        swapped = true;
       }
     }
-    input[indexForLowest] = input[i];
-    input[i] = lowestNumber
-  }
+  } while(swapped)
   return input;
 }
 
-console.log(selectionSort([33,2,52,106,73]))
-console.log(selectionSort([13,5,22,99,11]))
+console.log(selectionSort([33,2,52,106,73,5,16]))
+// console.log(selectionSort([13,5,22,99,11]))
