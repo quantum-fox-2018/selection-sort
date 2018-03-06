e6965d6e3259934d1c8958ca6691915eee1ca5d4
ihsanmaulanahamdani <ihsanmaulana5@gmail.com>
2018-03-06 15:14:20 +0700

    fixing-selection-sort

--- selectionSort.js
+++ selectionSort.js
@@ -4,12 +4,17 @@ const testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55];
 function selectionSort(arr) {
     // Your sorting code
     for (var i = 0; i < arr.length; i++) {
-        for (var j = 0; j < arr.length; j++) {
-            if (arr[i] < arr[j]) {
-                var numMin = arr[i];
-                arr[i] = arr[j];
-                arr[j] = numMin;
+        var min = i;
+        for (var j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
             }
+        }
+
+        if (i !== min) {
+            var temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
         }
     }
 
